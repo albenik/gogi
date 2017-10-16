20ee9ad1ab9a49066f881ad8bf512a2ba0e00775
Veniamin Albaev <albenik@gmail.com>
2017-10-16 17:40:25 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
+)
+
+func checkRepo(path string, mu *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	dir := filepath.Dir(path)
+	worktree := "--work-tree=" + dir
+	gitdir := "--git-dir=" + path
+
+	fetchcmd := exec.Command("git", worktree, gitdir, "fetch", "--all")
+	statuscmd := exec.Command("git", worktree, gitdir, "status", "--porcelain")
+	revlistcmd := exec.Command("git", worktree, gitdir, "rev-list", "HEAD@{upstream}..HEAD")
+
+	var fetchOut, statusOut, revlistOut []byte
+	var err error
+	if fetchOut, err = fetchcmd.CombinedOutput(); err != nil {
+		mu.Lock()
+		defer mu.Unlock()
+		fmt.Println(dir)
+		os.Stdout.Write(fetchOut)
+		fmt.Println("git fetch:", err)
+		return
+	}
+	if statusOut, err = statuscmd.CombinedOutput(); err != nil {
+		mu.Lock()
+		defer mu.Unlock()
+		fmt.Println(dir)
+		os.Stdout.Write(statusOut)
+		fmt.Println("git status:", err)
+		return
+	}
+	if len(statusOut) == 0 {
+		if revlistOut, err = revlistcmd.CombinedOutput(); err != nil {
+			mu.Lock()
+			defer mu.Unlock()
+			fmt.Println(dir)
+			os.Stdout.Write(revlistOut)
+			fmt.Println("git rev-list:", err)
+			return
+		}
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if len(statusOut) > 0 || len(revlistOut) > 0 {
+		fmt.Fprintln(os.Stdout, "\n", path)
+		os.Stdout.Write(statusOut)
+		os.Stdout.Write(revlistOut)
+	}
+	return
+}
+
+func mainfunc() int {
+	flag.Parse()
+
+	var (
+		err  error
+		root string
+		mu   sync.Mutex
+		wg   sync.WaitGroup
+	)
+
+	if flag.NArg() > 0 {
+		root = flag.Arg(0)
+	} else {
+		if root, err = os.Getwd(); err != nil {
+			fmt.Println(err)
+			return 1
+		}
+	}
+	if root, err = filepath.Abs(root); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	if _, err := os.Stat(root); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	err = filepath.Walk(root, func(path string, f os.FileInfo, err error) error {
+		if f.IsDir() && f.Name() == ".git" {
+			wg.Add(1)
+			go checkRepo(path, &mu, &wg)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return 1
+	}
+	wg.Wait()
+	return 0
+}
+
+func main() {
+	os.Exit(mainfunc())
+}
