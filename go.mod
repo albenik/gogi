905af0040960f6e43d04290fef637b05b6ed1d17
Veniamin Albaev <albenik@gmail.com>
2023-11-19 17:15:34 +0300

    wip

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/albenik/gogi
+
+go 1.15
+
+require (
+	github.com/Microsoft/go-winio v0.4.15 // indirect
+	github.com/go-git/go-billy/v5 v5.0.0
+	github.com/go-git/go-git/v5 v5.2.0
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
+	golang.org/x/net v0.0.0-20201201195509-5d6afe98e0b7 // indirect
+	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 
-	"github.com/albenik/gogi/vcs/git"
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/format/gitignore"
 )
 
 func mainf_() int {
@@ -50,7 +53,14 @@ func mainf_() int {
 		}
 		if f.IsDir() && f.Name() == ".git" {
 			wg.Add(1)
-			go git.NewRepo(path).Check(&wg, *fetchFlag)
+			go func(p string) {
+				defer wg.Done()
+				if err := check(p, *fetchFlag); err != nil {
+					fmt.Println()
+					fmt.Println("ERROR:", err)
+				}
+			}(filepath.Join(path, ".."))
+			// go g.NewRepo(path).Check(&wg, *fetchFlag)
 		}
 		return nil
 	})
@@ -64,4 +74,62 @@ func mainf_() int {
 
 func main() {
 	os.Exit(mainf_())
+}
+
+func check(path string, fetch bool) error {
+	repo, err := git.PlainOpen(path)
+	if err != nil {
+		return fmt.Errorf("cannot open repo %q: %w", path, err)
+	}
+
+	if fetch {
+		remotes, err := repo.Remotes()
+		if err != nil {
+			return fmt.Errorf("cannot get remotes %q: %w", path, err)
+		}
+
+		for _, r := range remotes {
+			err = r.Fetch(&git.FetchOptions{})
+			if err != nil && !errors.Is(err, git.NoErrAlreadyUpToDate) {
+				return fmt.Errorf("cannot fetch remote %q → %q: %w", path, r, err)
+			}
+		}
+	}
+
+	tree, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("cannot get worktree %q: %w", path, err)
+	}
+
+	status, err := tree.Status()
+	if err != nil {
+		return fmt.Errorf("cannot get status %q: %w", path, err)
+	}
+
+	ign, err := gitignore.LoadGlobalPatterns(osfs.New("/"))
+	if err != nil {
+		return fmt.Errorf("cannot read global gitinore file: %w", err)
+	}
+
+	fmt.Println("Status:")
+	for fn, fs := range status {
+		stat, err := os.Stat(fn)
+		if err != nil {
+			return fmt.Errorf("stat error: %q: %w", fn, err)
+		}
+		for _, i := range ign {
+			fmt.Println(fn, i, i.Match([]string{fn}, stat.IsDir()))
+			if i.Match([]string{fn}, stat.IsDir()) == gitignore.Exclude {
+				continue
+			}
+		}
+		fmt.Println(string(fs.Worktree), string(fs.Staging), fn)
+	}
+
+	// head, err := repo.Head()
+	// if err != nil {
+	// 	return fmt.Errorf("cannot get head %q: %w", path, err)
+	// }
+	// head.Name()
+	return nil
 }
